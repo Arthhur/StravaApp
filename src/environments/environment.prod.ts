428fbbeca29f56ef73f1b3dfacd0c632011a433c
arthhur <arthur.prodhomme@yahoo.fr>
2020-08-23 12:40:00 +0200

    change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  clientId: '25072',
+  clientSecret: 'f5f7e4be4cb88fb32ed966a19ee0bed8e98860a8',
+  token: 'cfea0dee1116518f1ca4ce7930f56ff0c7a9d45f'
 };
